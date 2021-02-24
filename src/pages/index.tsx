5c02c618d95b4336e1749c4a16cf9e32a7ac0f9c
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-02-23 22:17:21 -0300

    style complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,32 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { Experiencebar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from '../styles/Pages/Home.module.css';
+
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        Inicio | move.it
+      </Head>
+      
       <Experiencebar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
