9136ed14b130485e8be123b5bf21bf41ec812a0c
Felipe Henrique C. Ferreira <48157305+FelipeFerreiraDev@users.noreply.github.com>
2021-03-05 16:35:34 -0300

    Update

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: CountdownProviderProps) {
     const { startNewChallenge } = useContext(ChallengesContext);
 
-    const [time, setTime] = useState(20 * 60);
+    const [time, setTime] = useState(25 * 60);
 
     const [isActive, setIsActive] = useState(false);
 
@@ -37,7 +37,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     function resetCountdown() {
         clearTimeout(countdownTimeout);
         setIsActive(false);
-        setTime(20 * 60);
+        setTime(25 * 60);
         setHasFinished(false);
     }
 
